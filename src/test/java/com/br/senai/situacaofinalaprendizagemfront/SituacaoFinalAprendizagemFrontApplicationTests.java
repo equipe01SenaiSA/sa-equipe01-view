9939133e2198ee874dfc40f4eddf6f05495f0bea
kaylainevieira <kaylainevieira3@gmail.com>
2022-07-28 20:44:12 -0300

    first commit

---
+++ src/test/java/com/br/senai/situacaofinalaprendizagemfront/SituacaoFinalAprendizagemFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.senai.situacaofinalaprendizagemfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SituacaoFinalAprendizagemFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
