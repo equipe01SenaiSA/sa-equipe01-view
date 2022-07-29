22879379642d4f2d94de74ee603776f899d81369
kaylainevieira <kaylainevieira3@gmail.com>
2022-07-29 20:07:23 -0300

    Ajustando application.properties

--- src/main/java/com/br/senai/InitApp.java
+++ src/main/java/com/br/senai/InitApp.java
@@ -1,13 +1,52 @@
 package com.br.senai;
 
-import org.springframework.boot.SpringApplication;
+import java.awt.EventQueue;
+
+import javax.swing.JOptionPane;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.br.senai.view.TelaListagemCargo;
 
 @SpringBootApplication
 public class InitApp {
+	
+	@Autowired
+	private TelaListagemCargo listagemCargo;
 
 	public static void main(String[] args) {
-		SpringApplication.run(InitApp.class, args);
+		SpringApplicationBuilder builder = 
+				new SpringApplicationBuilder(InitApp.class);
+		builder.headless(false);
+		builder.run(args);
+	}
+	
+	@Bean	
+	public CommandLineRunner commandLineRunner(ApplicationContext ac) {
+		return args -> {
+			try {				
+				
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {							
+							//Apresentar tela principal aqui
+							listagemCargo.setVisible(true);
+						} catch (Exception e) {							
+							JOptionPane.showMessageDialog(null, e.getMessage());
+						}
+					}
+				});
+				
+			}catch (Exception e) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+			}
+			
+		};
 	}
 
 }
--- src/main/java/com/br/senai/view/TelaListagemCargo.java
+++ src/main/java/com/br/senai/view/TelaListagemCargo.java
@@ -10,20 +10,28 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.Serializable;
+import java.util.List;
 
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JButton;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.br.senai.client.CargoClient;
+import com.br.senai.dto.Cargo;
+import com.br.senai.view.table.CargoTableModel;
 
 @Component
 public class TelaListagemCargo extends JFrame implements Serializable {
@@ -36,146 +44,148 @@ public class TelaListagemCargo extends JFrame implements Serializable {
 	private CargoClient client;
 	
 	//@Autowired
-	//private TelaCadastroTransportadora cadastro;	
+	//private TelaCadastroCargo cadastro;	
 	
 	private JTextField edtDescricaoCurta;	
 	
-//	private void atualizar(JTable tabela) {
-//		List<Transportadora> transportadoras = client.listarPor(edtNomeFantasia.getText());		
-//		TransportadoraTableModel model = new TransportadoraTableModel(transportadoras);
-//		tabela.setModel(model);
-//		TableColumnModel cm = tabela.getColumnModel();
-//		cm.getColumn(0).setPreferredWidth(50);
-//		cm.getColumn(1).setPreferredWidth(352);
-//		tabela.updateUI();
-//	}
-//	
-//	private Transportadora getTransportadoraSelecionadaNa(JTable tabela) {
-//		int linhaSelecionada = tabela.getSelectedRow();
-//		if (linhaSelecionada < 0) {
-//			throw new IllegalArgumentException("Nenhuma linha foi selecionada");
-//		}
-//		TransportadoraTableModel model = (TransportadoraTableModel)tabela.getModel();
-//		Transportadora itemSelecionado = model.getPor(linhaSelecionada);
-//		return itemSelecionado;
-//	}
-//	
-//	private void removerRegistroDa(JTable tabela) {
-//		try {
-//			
-//			Transportadora transportadoraSelecionada = getTransportadoraSelecionadaNa(tabela);
-//			
-//			int opcaoSelecionada = JOptionPane.showConfirmDialog(
-//					contentPane, "Deseja remover?", "Remoção", JOptionPane.YES_NO_OPTION);
-//			
-//			if (opcaoSelecionada == JOptionPane.YES_OPTION) {			
-//				this.client.excluir(transportadoraSelecionada);
-//				((TransportadoraTableModel)tabela.getModel()).remover(transportadoraSelecionada);
-//				tabela.updateUI();
-//				JOptionPane.showMessageDialog(contentPane, "Transportadora removida com sucesso");
-//			}
-//			
-//		}catch (Exception e) {
-//			JOptionPane.showMessageDialog(contentPane, e.getMessage());
-//		}
-//	}
-//	
-//	private void editarRegistroDa(JTable tabela) {
-//		try {		
-//			Transportadora registroSelecionado = getTransportadoraSelecionadaNa(tabela);
-//			this.cadastro.colocarEmEdicao(registroSelecionado);
-//		}catch (Exception e) {
-//			JOptionPane.showMessageDialog(contentPane, e.getMessage());
-//		}
-//	}
-//
-//	/**
-//	 * Create the frame.
-//	 */
-//	public TelaListagemTransportadora() {
-//		setTitle("Listagem de Transportadora");
-//		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//		setBounds(100, 100, 450, 377);
-//		contentPane = new JPanel();
-//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-//		setContentPane(contentPane);
-//		
-//		JLabel lblNewLabel = new JLabel("Nome Fantasia");
-//		
-//		edtNomeFantasia = new JTextField();
-//		edtNomeFantasia.setColumns(10);
-//		
-//		JTable tabela = new JTable();
-//		tabela.setFillsViewportHeight(true);
-//		
-//		JButton btnListar = new JButton("Listar");
-//		btnListar.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				atualizar(tabela);
-//			}
-//		});			
-//		
-//		JScrollPane scrollPane = new JScrollPane(tabela);
-//		
-//		JButton btnEditar = new JButton("Editar");
-//		btnEditar.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				editarRegistroDa(tabela);
-//			}
-//		});
-//		
-//		JButton btnExcluir = new JButton("Excluir");
-//		btnExcluir.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				removerRegistroDa(tabela);
-//			}
-//		});
-//		
-//		JButton btnAdicionar = new JButton("Adicionar");
-//		btnAdicionar.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				cadastro.colocarEmInclusao();
-//			}
-//		});
-//		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-//		gl_contentPane.setHorizontalGroup(
-//			gl_contentPane.createParallelGroup(Alignment.LEADING)
-//				.addGroup(gl_contentPane.createSequentialGroup()
-//					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-//						.addComponent(lblNewLabel)
-//						.addComponent(edtNomeFantasia, GroupLayout.DEFAULT_SIZE, 412, Short.MAX_VALUE)
-//						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-//							.addContainerGap(242, Short.MAX_VALUE)
-//							.addComponent(btnAdicionar)
-//							.addPreferredGap(ComponentPlacement.RELATED)
-//							.addComponent(btnListar))
-//						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 413, GroupLayout.PREFERRED_SIZE)
-//						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-//							.addContainerGap(242, Short.MAX_VALUE)
-//							.addComponent(btnExcluir)
-//							.addPreferredGap(ComponentPlacement.RELATED)
-//							.addComponent(btnEditar)))
-//					.addContainerGap())
-//		);
-//		gl_contentPane.setVerticalGroup(
-//			gl_contentPane.createParallelGroup(Alignment.LEADING)
-//				.addGroup(gl_contentPane.createSequentialGroup()
-//					.addContainerGap()
-//					.addComponent(lblNewLabel)
-//					.addPreferredGap(ComponentPlacement.RELATED)
-//					.addComponent(edtNomeFantasia, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-//					.addPreferredGap(ComponentPlacement.RELATED)
-//					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-//						.addComponent(btnListar)
-//						.addComponent(btnAdicionar))
-//					.addPreferredGap(ComponentPlacement.RELATED)
-//					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)
-//					.addPreferredGap(ComponentPlacement.RELATED)
-//					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-//						.addComponent(btnEditar)
-//						.addComponent(btnExcluir))
-//					.addContainerGap(43, Short.MAX_VALUE))
-//		);
-//		contentPane.setLayout(gl_contentPane);
-//	}
+	private void atualizar(JTable tabela) {
+		List<Cargo> cargos = client.listarPor(edtDescricaoCurta.getText());		
+		CargoTableModel model = new CargoTableModel(cargos);
+		tabela.setModel(model);
+		TableColumnModel cm = tabela.getColumnModel();
+		cm.getColumn(0).setPreferredWidth(50);
+		cm.getColumn(1).setPreferredWidth(352);
+		tabela.updateUI();
+	}
+	
+	private Cargo getCargoSelecionadoNa(JTable tabela) {
+		int linhaSelecionada = tabela.getSelectedRow();
+		if (linhaSelecionada < 0) {
+			throw new IllegalArgumentException("Nenhuma linha foi selecionada");
+		}
+		CargoTableModel model = (CargoTableModel)tabela.getModel();
+		Cargo itemSelecionado = model.getPor(linhaSelecionada);
+		return itemSelecionado;
+	}
+	
+	private void removerRegistroDa(JTable tabela) {
+		try {
+			
+			Cargo cargoSelecionado = getCargoSelecionadoNa(tabela);
+			
+			int opcaoSelecionada = JOptionPane.showConfirmDialog(
+					contentPane, "Deseja remover?", "Remoção", JOptionPane.YES_NO_OPTION);
+			
+			if (opcaoSelecionada == JOptionPane.YES_OPTION) {			
+				this.client.excluir(cargoSelecionado);
+				((CargoTableModel)tabela.getModel()).remover(cargoSelecionado);
+				tabela.updateUI();
+				JOptionPane.showMessageDialog(contentPane, "Cargo removido com sucesso");
+			}
+			
+		}catch (Exception e) {
+			JOptionPane.showMessageDialog(contentPane, e.getMessage());
+		}
+	}
+	
+	private void editarRegistroDa(JTable tabela) {
+		try {		
+			Cargo registroSelecionado = getCargoSelecionadoNa(tabela);
+			//this.cadastro.colocarEmEdicao(registroSelecionado);
+		}catch (Exception e) {
+			JOptionPane.showMessageDialog(contentPane, e.getMessage());
+		}
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaListagemCargo() {
+		setTitle("Listagem de Cargo");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 377);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Filtro");
+		
+		edtDescricaoCurta = new JTextField();
+		edtDescricaoCurta.setColumns(10);
+		
+		JTable tabela = new JTable();
+		tabela.setFillsViewportHeight(true);
+		
+		JButton btnListar = new JButton("Listar");
+		btnListar.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				editarRegistroDa(tabela);
+			}
+		});
+		
+		JScrollPane scrollPane = new JScrollPane(tabela);
+		
+		JButton btnEditar = new JButton("Editar");
+		btnEditar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				editarRegistroDa(tabela);
+			}
+		});
+		
+		JButton btnExcluir = new JButton("Remover");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				removerRegistroDa(tabela);
+			}
+		});
+	
+		
+		JButton btnAdicionar = new JButton("Adicionar");
+		btnAdicionar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//cadastro.colocarEmInclusao();
+			}
+		});
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnListar)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 413, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(btnEditar)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnExcluir))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblNewLabel)
+							.addPreferredGap(ComponentPlacement.RELATED, 318, Short.MAX_VALUE)
+							.addComponent(btnAdicionar))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(edtDescricaoCurta, GroupLayout.DEFAULT_SIZE, 406, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(btnAdicionar))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(edtDescricaoCurta, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnListar)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnExcluir)
+						.addComponent(btnEditar)))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
 }
