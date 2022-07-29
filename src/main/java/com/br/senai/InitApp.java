a94e3c1d957827c42ae8358f96e41ea0d4e15ad6
kaylainevieira <kaylainevieira3@gmail.com>
2022-07-28 21:55:21 -0300

    Ajustando nome de arquivo

--- src/main/java/com/br/senai/InitApp.java
+++ src/main/java/com/br/senai/InitApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SituacaoFinalAprendizagemFrontApplication {
+public class InitApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SituacaoFinalAprendizagemFrontApplication.class, args);
+		SpringApplication.run(InitApp.class, args);
 	}
 
 }
